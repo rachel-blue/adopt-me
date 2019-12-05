b5b27bb649c21eb3bac50cc2b329872e4451bf70
Rachel Blue <rachel17.22@gmail.com>
2019-12-04 17:44:59 -0700

    error boundary and theme context added and implemented

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,7 +5,6 @@ class Carousel extends React.Component {
     photos: [],
     active: 0
   };
-
   static getDerivedStateFromProps({ media }) {
     let photos = ["http://placecorgi.com/600/600"];
 
@@ -22,18 +21,15 @@ class Carousel extends React.Component {
   };
   render() {
     const { photos, active } = this.state;
-
     return (
       <div className="carousel">
         <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
-            // @TODO - don't have event listner on an image in production.
             // eslint-disable-next-line
             <img
               key={photo}
               onClick={this.handleIndexClick}
-              // onKeyUp={this.handle}  @TODO - visible non-interactive elements with click handlers must have at least one keyboard listner
               data-index={index}
               src={photo}
               className={index === active ? "active" : ""}
